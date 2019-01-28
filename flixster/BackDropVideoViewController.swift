3091a41d18af41f7767a04ff25f47094ae26658d
Rodrigo leyva <leyvarodrigo36@yahoo.com>
2019-01-27 21:45:31 -0800

    added collection view

---
+++ flixster/BackDropVideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BackDropVideoViewController.swift
+//  flixster
+//
+//  Created by Rodrigo Leyva on 1/27/19.
+//  Copyright © 2019 Rodrigo Leyva. All rights reserved.
+//
+
+import UIKit
+
+class BackDropVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
